65c6d788e1bc33c50373cf123b6767aa578b1b5a
Cecilia Sandoval Medina <ceciliaud@gmail.com>
2026-02-19 19:44:12 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,11 @@
+
+
+URL_SERVICE = "https://cnt-f0887e83-70ce-4081-bfd2-e0569e4389f8.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ data.py
@@ -0,0 +1,10 @@
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Andrea",
+    "phone": "+11234567890",
+    "address": "123 Elm Street, Hilltop"
+}
